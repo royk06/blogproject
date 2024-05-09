16333ce471b3ecf8f84534d68eee3b24d1d1a978
royk <roykamau06@gmail.com>
2024-05-09 02:31:06 -0700

    navbar done

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = ()=>{
+    return (
+       
+        <div>
+            <nav>
+                <ul>
+                    <li href="">HOME</li>
+                    <li href="">CREATE</li>
+                </ul>
+            </nav>
+        </div>
+    );
+
+}
+export default Navbar
